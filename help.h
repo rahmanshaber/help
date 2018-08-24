d35168dcfcbbf47d505005fdb8c07c8433f800d0
s96Abrar <s96mini.cube@yahoo.com>
2018-08-25 00:19:01 +0600

    Fixed path problem.

--- help.h
+++ help.h
@@ -22,7 +22,7 @@ along with this program; if not, see {http://www.gnu.org/licenses/}. */
 #include <QTextStream>
 #include <QFile>
 
-#include "utilities.h"
+#include <cprime/utilities.h>
 
 
 namespace Ui {
